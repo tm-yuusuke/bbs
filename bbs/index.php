34631b0f7b7e64e907916f79cca8e1a5f15de329
tm-yuusuke <82048717+tm-yuusuke@users.noreply.github.com>
2021-05-06 16:13:34 +0900

    Add files via upload

---
+++ bbs/index.php
@@ -0,0 +1,127 @@
+<?php
+session_start();
+require('dbconnect.php');
+
+
+  if(isset($_SESSION['id']) && $_SESSION['time'] + 3600 > time()){
+    $_SESSION['time'] = time();//アクションを起こした時間を更新して自動ログアウトしないようにする
+  
+    $members = $db->prepare('SELECT * FROM members WHERE id=?');
+    $members ->execute(array($_SESSION['id']));
+    $member  = $members->fetch();
+  }else{
+    header('Location: login.php');
+    exit();
+  }//ログインしていない状態からアクセスするとログイン画面に強制的に移動させる。
+
+  if(!empty($_POST)){//$_POSTが空ではないとき。つまり投稿するボタンが押されたとき。
+    if($_POST['message'] !==''){//テキストボックスエリアが空白で無いとき。
+      if($_POST["reply_post_id"] == "") 
+      $_POST["reply_post_id"] = 0;
+      
+      $message = $db->prepare('INSERT INTO posts SET member_id=?, message=?, reply_message_id=?, created=NOW()');
+      $message->execute(array(
+       $member['id'],
+       $_POST['message'],
+       $_POST['reply_post_id']
+      ));
+
+      header('Location: index.php');//この処理をしないと$_POSTにメッセージが残ってるので、更新を押すとテキストエリアが空白なのに同じメッセージが重複して登録される。
+      exit();
+    }
+  }
+//ページネーションの処理
+$page = $_REQUEST['page'];
+
+  if($page == ''){
+    $page = 1;
+  }
+
+$page = max($page,1);//max関数。$pageと1を比べ、1の方が大きい時は1を入れる
+
+$counts = $db->query('SELECT COUNT(*) AS cnt FROM posts');
+$cnt = $counts->fetch();
+$maxPage = ceil($cnt['cnt'] / 5);//dbからcountした件数を5で割り少数は切り上げて最大ページ数にする。例えば10件なら10÷5=2ページ13件なら13÷5=2.6切り上げて3ページ
+$page = min($page, $maxPage);//maxpageと入力されたページ数を比較して、入力が大きかったらmaxpageを入れる
+$start = ($page - 1) * 5;//１ページ目だと(1-1)×5で0から5件。2ページ目だと(2-1)×5=5で5から10件を取得
+
+$posts = $db->prepare('SELECT m.name, m.picture, p.* FROM members m, posts p WHERE m.id=p.member_id ORDER BY p.created DESC LIMIT ?,5');
+$posts->bindParam(1, $start, PDO::PARAM_INT);//LIMITの?は数字が入る必要がある。executeにすると文字として入るので、bindParamで数字に縛る
+$posts->execute();
+//ページネーションの処理
+
+  if(isset($_REQUEST['res'])){
+    //返信の処理
+    $response = $db->prepare('SELECT m.name,m.picture, p.* FROM members m,posts p WHERE m.id=p.member_id AND p.id=?');
+    $response ->execute(array($_REQUEST['res']));
+
+    $table = $response->fetch();
+    $message = '@' . $table['name'] . ' ' . $table['message'];
+  }//メッセージが取得できないときの処理を追加する
+  //ログインが成功した際セッションidとtimeが付与されるので、セッションidでログイン状態を判別する
+?>
+<!--ここまででphpの処理終了。以下htmlの表示-->
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>ひとこと掲示板</title>
+
+	<link rel="stylesheet" href="style.css" />
+</head>
+
+<body>
+<div id="wrap">
+    <div id="head">
+      <h1>ひとこと掲示板</h1>
+    </div><!--id="head"の閉じdiv-->
+  <div id="content">
+  	<div style="text-align: right"><a href="logout.php">ログアウト</a></div><!--ログアウトの閉じdiv-->
+      <form action="" method="post">
+        <dl>
+          <dt><?php print(htmlspecialchars($member['name'], ENT_QUOTES));?>さん、メッセージをどうぞ</dt>
+          <dd>
+           <textarea name="message" cols="50" rows="5"><?php print(htmlspecialchars($message, ENT_QUOTES)); ?></textarea>
+           <input type="hidden" name="reply_post_id" value="<?php print(htmlspecialchars($_REQUEST['res'], ENT_QUOTES)); ?>" />
+          </dd>
+        </dl>
+        <div>
+          <p><input type="submit" value="投稿する" /></p>
+       </div><!--投稿するの閉じdiv-->
+      </form>
+
+      <?php foreach ($posts as $post): ?>
+        <div class="msg"><!--ここのdivタグで一覧を表示する。-->
+          <img src="member_picture/<?php print(htmlspecialchars($post['picture'], ENT_QUOTES)); ?>" width="48" height="48" alt="<?php print(htmlspecialchars($post['name'], ENT_QUOTES)); ?>" />
+          <p><?php print(htmlspecialchars($post['message'], ENT_QUOTES)); ?><span class="name">（<?php print(htmlspecialchars($post['name'], ENT_QUOTES)); ?>）</span>[<a href="index.php?res=<?php print(htmlspecialchars($post['id'], ENT_QUOTES)); ?>">Re</a>]</p><!--$postはdbから取ってきた$postsを代入したもの-->
+            <p class="day"><a href="view.php?id=<?php print(htmlspecialchars($post['id'])); ?>"><?php print(htmlspecialchars($post['created'], ENT_QUOTES)); ?></a>
+              <?php if ($post['reply_message_id'] > 0): ?>
+               <a href="view.php?id=<?php print(htmlspecialchars($post['reply_message_id'], ENT_QUOTES)); ?>">返信元のメッセージ</a>
+              <?php endif; ?>
+
+             <?php if($_SESSION['id'] == $post['member_id']): ?>
+              [<a href="delete.php?id=<?php print(htmlspecialchars($post['id'])); ?>"style="color: #F33;">削除</a>]
+              <?php endif; ?>
+            </p><!--p class="day"の閉じp-->
+        </div><!--class="msg"の閉じdiv-->
+      <?php endforeach; ?>
+
+    <ul class="paging">
+      <?php if($page > 1): ?>
+        <li><a href="index.php?page=<?php print($page-1);?>">前のページへ</a></li>
+       <?php else: ?>
+       <li>前のページへ</li>
+      <?php endif; ?>
+
+      <?php if($page < $maxPage): ?>
+       <li><a href="index.php?page=<?php print($page+1);?>">次のページへ</a></li>
+       <?php else: ?>
+       <li>次のページへ</li>
+      <?php endif; ?>
+    </ul>
+  </div><!--id="contact"の閉じdiv-->
+</div><!--id="wrap"の閉じdiv-->
+</body>
+</html>
---
+++ bbs/login.php
@@ -0,0 +1,105 @@
+<?php
+session_start();
+require('dbconnect.php');
+
+  //emailをcookieに入れ、自動ログインできるようにする
+  if($_COOKIE['email'] !== ''){
+    $email = $_COOKIE['email'];
+  }
+  //$_POSTの中身はユーザーが入力した<input type>タグのname属性の値
+  //echo($_POST['email']);エコーで渡ってきた$_POSTの値を確認
+  //echo($_POST['password']);$_POSTのpasswordの値を確認
+  if(!empty($_POST)){
+       //クッキーを使うので、$emailをPOST[email]で上書きする
+      $email = $_POST['email'];
+      //idで拾ってくる。$_POST['id']とSQLのWHERE id=?でidが一緒の人の行を引っ張ってきてverify($_POST['pass',db内のパス])
+      $hashpass = $db->prepare('SELECT password FROM members WHERE email=?');
+      //ログインしようとするユーザーのid(登録時に自動的に割り振られるid)を箱(配列)に入れて$hashpassに入れる。
+      $hashpass->execute(array($_POST['email']));
+      //$_POST['id']はない。
+      //fetchしないと実際のデータは値で変数に入らない。
+      $hash = $hashpass->fetch();
+      //var_dump($hash);
+      if($_POST['email'] !=='' && $_POST['password'] !=='' && password_verify(htmlspecialchars($_POST['password'],ENT_QUOTES),$hash['password'])){
+        //echo ($hash['password']);
+        $login = $db->prepare('SELECT * FROM members WHERE email=? AND password=?');
+        //正しいログインメールアドレスとパスワードが入力されたらデータベースに値を取得しに行く。DBのpassはハッシュ後pass
+        $login->execute(array($_POST['email'],$hash['password']));
+        //$loginに対するexecuteも、DBのemailとpasswordが入る。つまり、パスワードは$hashのパスワードを入れる必要がある。
+        //もしフェッチで情報が１件帰ってきたらログインに成功している。１件も返ってこないならログインに失敗している。
+        //fetchにて、DBから全件取得した行を１行づつ$memberに入れる。
+        $member = $login->fetch();
+          if($member){
+            //$_SESSIONの中は$_POST(name,email,password,image)の４つ。ここの$_SESSIONはPOSTの中身ではなく、SESSION_id
+            $_SESSION['id'] = $member['id'];
+            $_SESSION['time'] = time();
+            
+              //次回から自動入力のチェックボックス。value=onならcookie内のemailを自動でセット
+              if($_POST['save'] === "on"){
+                setcookie('email', $_POST['email'], time()+60*60*24*14);
+              }
+            
+            header('Location: index.php');
+            exit();
+          }else{
+            //fetchにデータが返ってこなかったら
+            $error['login'] = 'failed';
+          }
+      }else{
+        //ネームかパスワードが空なら
+        $error['login'] = 'blank';
+      }
+  }
+?>
+
+<!DOCTYPE html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link rel="stylesheet" type="text/css" href="style.css" />
+<title>ログインする</title>
+</head>
+
+<body>
+<div id="wrap">
+  <div id="head">
+    <h1>ログインする</h1>
+  </div>
+  <div id="content">
+    <div id="lead">
+      <p>メールアドレスとパスワードを記入してログインしてください。</p>
+      <p>入会手続きがまだの方はこちらからどうぞ。</p>
+      <p>&raquo;<a href="join/index_join.php">入会手続きをする</a></p>
+    </div>
+    <form action="" method="post">
+      <dl>
+        <dt>メールアドレス</dt>
+        <dd>
+          <input type="text" name="email" size="35" maxlength="255" value="<?php print(htmlspecialchars($email, ENT_QUOTES)); ?>" />
+          <?php if ($error['login'] === 'blank'): ?>
+            <P class="error">*メールアドレスとパスワードを入力して下さい</P>
+          <?php endif; ?>
+          <?php if ($error['login'] === 'failed'): ?>
+            <P class="error">*ログインに失敗しました。正しくご記入下さい</P>
+          <?php endif; ?>
+        </dd>
+        <dt>パスワード</dt>
+        <dd>
+          <input type="password" name="password" size="35" maxlength="255" value="<?php print(htmlspecialchars($_POST['password'], ENT_QUOTES)); ?>" /><!--メソッドの引数の設定の仕方に注意。第一引数とENT-->
+        </dd>
+        <dt>ログイン情報の記録</dt>
+        <dd>
+          <input id="save" type="checkbox" name="save" value="on">
+          <label for="save">次回からは自動的にログインする</label>
+        </dd>
+      </dl>
+      <div>
+        <input type="submit" value="ログインする" />
+      </div>
+    </form>
+  </div>
+  <div id="foot">
+    <p></p>
+  </div>
+</div>
+</body>
+</html>
